74ff02277730ee5033ec7c11136acd00e1f92c38
palenq <palenq@gmail.com>
2023-03-13 02:38:09 -0700

    Update chat_with_chatgpt.py

--- source/chat_with_chatgpt.py
+++ source/chat_with_chatgpt.py
@@ -2,19 +2,23 @@ import os
 import openai
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-gpt_prompt = "Correct this to standard English:\n\nShe no went to the market."
+try:
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+  gpt_prompt = "Correct this to standard English:\n\nShe no went to the market."
 
 
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=gpt_prompt,
-  temperature=0.5,
-  max_tokens=256,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+  response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=gpt_prompt,
+    temperature=0.5,
+    max_tokens=256,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
 
 
-print(response['choices'][0]['text'])
+    print(response['choices'][0]['text'])
+
+  except Exception as e: 
+    print(e)
